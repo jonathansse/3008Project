9b8d26e356ad9690bf414f182899141233ae67ef
Ismail <daheeye1011@gmail.com>
2023-11-01 21:22:26 -0400

    final changes

--- server.js
+++ server.js
@@ -2,18 +2,120 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let Itinerary = {
+	restaurants: {
+		"Cheap Eats": {
+			0: {
+				name: "Arepera",
+				description: "Venezuelan",
+				address: "73 Rue Prince-Arthur E",
+				price: 15,
+				image: "restaurants/Arepera.jpg"
+	
+			},
+			1: {
+				name: "Restaurant Dobe & Andy",
+				description: "Chinese",
+				address: "1071 Rue Saint-Urbain R-12",
+				price: 18,
+				image: "restaurants/restaurantDobe&Andy.jpg"
+			}
+		},
+		"Moderate": {
+			2: {
+				name: "Kazu",
+				description: "Japanese",
+				address: "1844 Saint-Catherine St W",
+				price: 40,
+				image: "restaurants/Kazu.jpg"
+			},
+			3: {
+				name: "Venice Mtl Vieux-Montreal",
+				description: "Californian",
+				address: "440 Saint Francois Xavier St",
+				price: 48,
+				image: "restaurants/veniceMtlVieux-Montreal.jpg"
+			}
+		},
+		"Expensive": {
+			4: {
+				name: "L'Express",
+				description: "French",
+				address: "3927 Saint Denis St",
+				price: 75,
+				image: "restaurants/l'Express.jpg"
+			}
+		}
+	},
+	activities: {
+		"Family friendly": {
+			5: {
+				name: "Parc Mont-Royal",
+				description: "A vast park in the heart of Montreal, perfect for outdoor activities.",
+				address: "2000 Chemin Remembrance Montréal, QC H3H 1A2",
+				price: 23,
+				image: "activities/parcMontRoyal.jpg"
+			},
+			6: {
+				name: "SOS Labyrinthe",
+				description: "A thrilling maze adventure at the Old Port.",
+				address: "Clock Tower Pier Old Port of Montréal Montréal, QC H2Y 0B4",
+				price: 23,
+				image: "activities/SOSLabyrinthe.jpg"
+			},
+			7: {
+				name: "Pirates or Privateers?",
+				description: "Experience pirate life with interactive exhibits.",
+				address: "Pointe-à-Callière, Montréal Archaeology and History Complex 350 Place Royale Montréal, QC",
+				price: 26,
+				image: "activities/piratesOrPrivateers.jpg"
+			}
+		},
+		"Non-Family friendly": {
+			8: {
+				name: "Casino De Montreal",
+				description: "Casino", 
+				address: "1 Ave du Casino, Montreal, QC H3C 4W7",
+				price: 150,
+				image: "activities/casinoDeMontreal.jpg"		
+			},
+			9: {
+				name: "Ninja Warrior et Parkour à Montreal",
+				description: "indoor activity and family friendly",
+				address: "2350 Rue Dickson Local 340, Montréal, QC H1N 3T1",
+				price: 30,
+				image: "activities/ninjaWarrior.jpg"
+			}
+		}
+		
+	}	
+}
+
+let order = {};
+
+let budget = 0, subtotal = 0, total = 0, tax = 0;
+
 app.use(express.static('public'));
 app.use(express.json());
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+
+
+app.get('/orders', (req, res) => {
+    res.status(200).json([Itinerary, order, ]);
 });
 
-const selectedItems = [];
+// saves the user's order to order collection.
+app.post("/orders", (req, res) => {
+    subtotal = req.body.subtotal, total = req.body.total,
+    tax = req.body.tax, order = req.body.order;
+	console.log(order);
+	res.status(200).send();
+});
 
-app.post('/add-item', (req, res) => {
-    const { category, item } = req.body;
-    const newItem = { category, item };
-    selectedItems.push(newItem);
-    res.json(newItem);
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
